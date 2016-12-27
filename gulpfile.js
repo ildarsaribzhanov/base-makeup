38ff34348693c1cbacdc28fb440e05e4d5feef52
Ildar Saribzhanov <ildar@sawtech.ru>
2016-12-27 09:50:42 +0300

    Update makeup for Gulp

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var gulp       = require('gulp'),
+    sass       = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify     = require('gulp-uglify'),
+    rename     = require('gulp-rename');
+
+// SCSS compiled
+gulp.task('sass', function () {
+	return gulp.src('./css/**/*.scss')
+
+	// Для генераии style.css.map
+		.pipe(sourcemaps.init())
+
+		// Собственно компиляция
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+
+		// Куда положить .map
+		.pipe(sourcemaps.write('./'))
+
+		// Путь компиляции
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+	gulp.watch('./css/**/*.scss', ['sass']);
+});
+
+
+// JS COMPRESSED
+gulp.task('compressed-js', function () {
+	gulp.src('js/src/*.js')
+		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('js'));
+});
+
+gulp.task('compressed-js:watch', function () {
+	gulp.watch('./js/src/*.js', ['compressed-js']);
+});
+
+
+// Смотрим на все
+gulp.task('watch', ['sass:watch', 'compressed-js:watch']);
