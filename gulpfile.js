b1a1df04e8911465c534e59e54e7b3cc09858a13
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2018-05-03 14:27:24 +0300

    [Update] compile all
    Вывод попапов об ошибках, ES6, константы для конфигов, задача выполнения всего

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,64 @@
 'use strict';
 
-var gulp       = require('gulp'),
+let gulp       = require('gulp'),
     sass       = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify     = require('gulp-uglify'),
     rename     = require('gulp-rename'),
-    imagemin   = require('gulp-imagemin');
+    imagemin   = require('gulp-imagemin'),
+    plumber    = require('gulp-plumber'),
+    babel      = require('gulp-babel'),
+    notify     = require('gulp-notify');
+
+const PATHS = {
+	css  : {
+		src: './css/**/*.scss',
+		build : './css'
+	},
+	js: {
+		src: './js/src/*.js',
+		build : './js/build'
+	},
+	img: {
+		src: './images/*',
+		build : './images'
+	}
+};
 
 // SCSS compiled
 gulp.task('sass', function () {
-	return gulp.src('./css/**/*.scss')
+	return gulp.src(PATHS.css.src)
+	// отлов ошибок
+		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
 
-	// Для генераии style.css.map
+		// Для генераии style.css.map
 		.pipe(sourcemaps.init())
 
 		// Собственно компиляция
-		.pipe(sass({outputStyle: 'expanded', indentType: 'tab', indentWidth: 1}).on('error', sass.logError))
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
 
 		// Куда положить .map
 		.pipe(sourcemaps.write('./'))
 
 		// Путь компиляции
-		.pipe(gulp.dest('./css'));
+		.pipe(gulp.dest(PATHS.css.build));
 });
 
 gulp.task('sass:watch', function () {
-	gulp.watch('./css/**/*.scss', ['sass']);
+	gulp.watch(PATHS.src.css, ['sass']);
 });
 
 // image compress
-gulp.task('compress-img', function() {
-	gulp.src('images/*')
+gulp.task('compress-img', function () {
+	gulp.src(PATHS.img.src)
 		.pipe(imagemin())
-		.pipe(gulp.dest('images'))
+		.pipe(gulp.dest(PATHS.img.build))
 });
 
 
 // JS COMPRESSED
 gulp.task('compressed-js', function () {
-	gulp.src('js/src/*.js')
+	gulp.src(PATHS.js.src)
 		.pipe(sourcemaps.init())
 
 		// Собственно минификация
@@ -48,16 +68,18 @@ gulp.task('compressed-js', function () {
 		.pipe(rename({suffix: '.min'}))
 
 		// map-файл положим в отдельную папку
-		.pipe(sourcemaps.write('./maps'))
+		.pipe(sourcemaps.write('./'))
 
 		// А результат сжатия тоже в свою папку
-		.pipe(gulp.dest('js'));
+		.pipe(gulp.dest(PATHS.js.build));
 });
 
 gulp.task('compressed-js:watch', function () {
-	gulp.watch('./js/src/*.js', ['compressed-js']);
+	gulp.watch(PATHS.js.src, ['compressed-js']);
 });
 
+// Смотрим на все
+gulp.task('do-all', ['sass', 'compressed-js']);
 
 // Смотрим на все
 gulp.task('watch', ['sass:watch', 'compressed-js:watch']);
